215b766fd9fb6aa49c61b0619f1ab16ab0e40085
DMoncayo999 <delfina.moncayo@gmail.com>
2024-03-26 23:07:54 -0400

    Finish modules activity wk05

---
+++ week05/modules.js
@@ -0,0 +1,16 @@
+//modules.js
+import aCourse from "./Course.js";
+  
+  aCourse.init();
+
+  document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum);
+  });
+
+  document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum, false);
+  });
+  
+ 
